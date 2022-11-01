3d21abb0e37f7472c391e6009823444ab1325bb8
949027 <82878668+949027@users.noreply.github.com>
2022-11-01 16:20:12 +0700

    New app. Models added

--- class_organizer/admin.py
+++ class_organizer/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from class_organizer.models import Curator, Student, Course, Group, Subject
 
-# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Curator)
+class CuratorAdmin(admin.ModelAdmin):
+    pass
+
+
+class GroupInline(admin.TabularInline):
+    model = Group
+    #readonly_fields = ('gender', )
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [GroupInline]
+
+
+class StudentInline(admin.TabularInline):
+    model = Student
+    #readonly_fields = ('gender', )
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    inlines = [StudentInline]
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    pass
--- class_organizer/models.py
+++ class_organizer/models.py
@@ -1,3 +1,80 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Curator(models.Model):
+    user = models.ForeignKey(
+        User,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.user.username
+
+
+class Student(models.Model):
+    user = models.ForeignKey(
+        User,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE,
+    )
+    gender = models.CharField(
+        'Пол',
+        max_length=10,
+        choices=[('male', 'Мужской'), ('female', 'Женский')],
+    )
+    group = models.ForeignKey(
+        'Group',
+        verbose_name='Группа',
+        related_name='students',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    def save(self, *args, **kwargs):
+        if self.group.students.count() < 2:
+            return super().save(*args, **kwargs)
+        raise ValidationError(
+            'There should be no more than 20 people in the group',
+        )
+
+    def __str__(self):
+        return self.user.username
+
+
+class Group(models.Model):
+    name = models.CharField('Наименование группы', max_length=100)
+    subjects = models.ManyToManyField('Subject', verbose_name='Дисциплины')
+    course = models.ForeignKey(
+        'Course',
+        verbose_name='Направление',
+        on_delete=models.PROTECT,
+
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    name = models.CharField('Наименование направления', max_length=100)
+    curator = models.OneToOneField(
+        Curator,
+        verbose_name='Куратор',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Subject(models.Model):
+    name = models.CharField('Наименование дисциплины', max_length=100)
+
+    def __str__(self):
+        return self.name
