8b7724f162085991d641ea12cabb9050cd03c0a6
949027 <82878668+949027@users.noreply.github.com>
2022-11-02 00:23:40 +0700

    DRF included

--- university/settings.py
+++ university/settings.py
@@ -38,7 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'class_organizer',
+    'api',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
